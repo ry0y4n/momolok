37c6d14acc73266fdf89953a19d4f68ea8158b5c
ry0y4n <rusk.ryoy4n@gmail.com>
2021-02-21 20:05:00 +0900

    add: サーバーモード(API化)実装

--- index.js
+++ index.js
@@ -1,14 +1,74 @@
 const pavlok = require('pavlok');
+const express = require('express')
+
+const app = express();
 
 pavlok.init(
     "f421bc3ab16596d574b334578d379c98e500db565eccdbd7adbe0e20f32a14ea",
-    "f3fe1e1a2f78f46dd345d4d63110347e5fb2e8d12624ed555cdb6e3e81d25fd5"
+    "f3fe1e1a2f78f46dd345d4d63110347e5fb2e8d12624ed555cdb6e3e81d25fd5",
+    {
+        "verbose": true,
+        "app" : app,
+        "message": "Hello from the Pavlok Remote example!",
+        "callbackUrl": "http://localhost:3000/auth/pavlok/result",
+        "callbackUrlPath": "/auth/pavlok/result",
+        "successUrl": "/api/v1/auth/success",
+        "errorUrl": "/api/v1/auth/error"
+    }
 );
 
-pavlok.login(function (result, code) {
-    if (result) {
-        console.log("Code is " + code);
-        pavlok.vibrate(); // or you can call other methods like beep() or zap()
-        pavlok.beep();
+app.get("/", function(req, result){
+	if(pavlok.isLoggedIn(req)){
+		result.sendFile(__dirname + "/main.html");
+	} else {
+   		result.redirect("login.html");
+	}
+});
+app.get("/api/v1/auth/login", function(req, result){
+	pavlok.auth(req, result);
+});
+
+app.get("/zap", function(req, result){
+	pavlok.zap({
+		"request": req
+	});
+	console.log("Zapped!");
+	result.sendFile(__dirname + "/main.html");
+});
+app.get("/vibrate", function(req, result){
+	pavlok.vibrate({
+		"request": req
+	});
+	console.log("Vibrated!");
+	result.sendFile(__dirname + "/main.html");
+});
+app.get("/beep", function(req, result){
+	pavlok.beep({
+		"request": req
+	});
+	console.log("Beeped!");
+	result.sendFile(__dirname + "/main.html");
+});
+
+app.get("/pattern", function(req, result){
+	pavlok.pattern({
+		"request": req,
+		"pattern": [ "beep", "vibrate", "zap" ],
+		"count": 2
+	});
+	console.log("Pattern'd!");
+	result.sendFile(__dirname + "/main.html");
+});
+app.get("/logout", function(req, result){
+	pavlok.logout(req);
+	result.redirect("/");	
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log("Failed to start the server");
+        console.log(err)
+    } else {
+        console.log("Visit the IP address of this machine, or http://localhost:3000/.");
     }
 });
